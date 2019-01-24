992ccc0aa734216eaa089c7b8b84b94cb5a52da5
jeffer.ng <jeffer.ng@MU-C1-14.mullen.pvt.k12.co.us>
2019-01-24 09:11:59 -0700

    fixed the out of bounds issue

--- Group Creator/src/GroupAssigner.java
+++ Group Creator/src/GroupAssigner.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GroupAssigner
 	{
 		static Scanner userInput = new Scanner(System.in);
 		static int amount;
 		static String[] groupHistory;
-		static String[] groups;
+		static String[] groups = new String[amount];
+		static ArrayList<String> students = new ArrayList<String>();
 		
 		public static void main(String[] args) throws IOException
 			{
@@ -21,18 +23,42 @@ public class GroupAssigner
 			{
 				Scanner studentNames = new Scanner(new File("StudentList.txt"));
 				System.out.println("Welcome to the Group Creator 2020"
-						+ "\nHow many people do you want per group?");
+						+ "\nHow many groups would you like to have?");
 				amount = userInput.nextInt();
-				
-				
+				while(studentNames.hasNext())
+					{
+						String temp = studentNames.nextLine();
+						String[] temps = temp.split(" ");
+						students.add(temps[0] + " " + temps[1]);
+						
+						
+					}
+				System.out.println(students);
 				
 			}
 
-		private static void assigningGroup() throws IOException
+		private static void assigningGroup()
 			{
+				int random = (int) (Math.random()*students.size() - 1);
+				int[] history = new int[students.size()];
 				
-				
-				
+				for(int i = 0; i < groups.length; i++)
+					{
+						history[i] = random;
+//						if(i == 0)
+//							{
+//								groups[i] = students.get(random);
+//							}
+//						else if(i > 0 && history[i - 1] != random)
+//							{
+								groups[i] = students.get(random);
+//							}
+//						else
+//							{
+//								i--;
+//							}
+						
+					}
 				
 				
 			}
